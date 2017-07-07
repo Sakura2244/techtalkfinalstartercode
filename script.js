c0cbb079daf8477d90cd077f371c457e567b6155
Jennifer <jenniferzheng23@gmail.com>
2017-07-07 19:24:37 +0000

    my website

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // JavaScript File
-// JavaScript File
-$( document ).ready(function() {
 
+$( document ).ready(function() {
+    $("#sakura").click(function(){
+    for(var count = 0; count < 5; count = count + 1){
+        $("p").append('<img src="https://i.ytimg.com/vi/dCTpZcMZz4M/maxresdefault.jpg">');
+    }
+});
+$("question").click(function(){
+    var answer = $("#answer").val();
+    if ( answer==="Sakura"){
+        $("body").html("Correct!");
+    }else if ( answer==="sakura"){
+        $("body").html("Correct!");
+    } else {
+        $("body").html("Incorrect!!!");
+    }
+});
 
 });
